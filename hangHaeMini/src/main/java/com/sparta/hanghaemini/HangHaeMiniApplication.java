8b2f0a66acf0ff375cb1cda4d7f2267b3922f3be
JooLegend <ehenr2003@naver.com>
2022-10-21 10:21:31 +0900

    미니프로젝트

---
+++ hangHaeMini/src/main/java/com/sparta/hanghaemini/HangHaeMiniApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.hanghaemini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangHaeMiniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HangHaeMiniApplication.class, args);
+    }
+
+}
