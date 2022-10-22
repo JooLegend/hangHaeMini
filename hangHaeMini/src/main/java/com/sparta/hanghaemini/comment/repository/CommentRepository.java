bb826238b21b2bc8438f3fcffede7d3ac6ea8327
meruberu <wmj0924@gmail.com>
2022-10-22 20:25:27 +0900

    Comment Repository 수정

--- hangHaeMini/src/main/java/com/sparta/hanghaemini/comment/repository/CommentRepository.java
+++ hangHaeMini/src/main/java/com/sparta/hanghaemini/comment/repository/CommentRepository.java
@@ -4,7 +4,7 @@ import com.sparta.hanghaemini.account.entity.Account;
 import com.sparta.hanghaemini.comment.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public class CommentRepository  extends JpaRepository<Comment, Long> {
+public interface CommentRepository  extends JpaRepository<Comment, Long> {
 
 
     Comment findByCommentIdAndPost_Id(Long commentid,Long postid);
