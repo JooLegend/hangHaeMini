ce7aa8c824a2bf3cceffbe7b19163742eae94647
GGuiGui <rbqjachl95@gmail.com>
2022-10-23 19:58:57 +0900

    cors 오늘 마지막 시도!!! 이거 안되면 나 못해

--- hangHaeMini/src/main/java/com/sparta/hanghaemini/security/WebSecurityConfig.java
+++ hangHaeMini/src/main/java/com/sparta/hanghaemini/security/WebSecurityConfig.java
@@ -22,6 +22,8 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.List;
+
 @Configuration
 @EnableWebSecurity
 @ConditionalOnDefaultWebSecurity
@@ -63,9 +65,9 @@ public class WebSecurityConfig{
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.addAllowedOriginPattern("*");
-        configuration.addAllowedHeader("*");
-        configuration.addAllowedMethod("*");
+        configuration.setAllowedOriginPatterns(List.of("*"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PATCH", "DELETE", "PUT", "HEAD"));
+        configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
