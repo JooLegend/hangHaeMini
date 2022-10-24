571d0f6e799b3164da67bb43fdc5e03a45aaa072
GGuiGui <rbqjachl95@gmail.com>
2022-10-24 15:00:58 +0900

    10차시도

--- hangHaeMini/src/main/java/com/sparta/hanghaemini/common/RestExceptionHandler.java
+++ hangHaeMini/src/main/java/com/sparta/hanghaemini/common/RestExceptionHandler.java
@@ -1,4 +1,16 @@
-package com.sparta.hanghaemini.common;
-
-public class RestExceptionHandler {
-}
+//package com.sparta.hanghaemini.common;
+//
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.ExceptionHandler;
+//import org.springframework.web.bind.annotation.RestControllerAdvice;
+//
+//@RestControllerAdvice
+//public class RestExceptionHandler {
+//    @ExceptionHandler(RuntimeException.class)
+//    public ResponseEntity<?> exceptionHandler(RuntimeException e){
+//        return ResponseEntity
+//                .status(HttpStatus.BAD_REQUEST)
+//                .body(CommonResponseDto.fail(e.getMessage()));
+//    }
+//}
--- hangHaeMini/src/main/java/com/sparta/hanghaemini/security/WebSecurityConfig.java
+++ hangHaeMini/src/main/java/com/sparta/hanghaemini/security/WebSecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.security.ConditionalOnDefaultWebSe
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
