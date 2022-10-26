20cd2ce981deab04fb60e2f1dba62afb759fd694
GGuiGui <rbqjachl95@gmail.com>
2022-10-26 15:31:59 +0900

    프론트와 주소 맞추기 및 nickname 입력

--- hangHaeMini/src/main/java/com/sparta/hanghaemini/comment/controller/CommentController.java
+++ hangHaeMini/src/main/java/com/sparta/hanghaemini/comment/controller/CommentController.java
@@ -11,7 +11,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 
-@RequestMapping("/api")
+@RequestMapping
 @RestController
 @RequiredArgsConstructor
 class CommentController {
